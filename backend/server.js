a87ea75f29618242599ca52a7fb0f0020340bce2
Jatin Rajput <jatinrjpt21@gmail.com>
2025-04-01 20:43:46 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://future-you-4th1.vercel.app/',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
